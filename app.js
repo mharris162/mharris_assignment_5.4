f37ad41a05afdf3ffd52967e2ffbbe97a7833eb0
mharris162 <mharris162@student.alamo.edu>
2024-09-23 17:45:43 -0500

    for and while loops

---
+++ app.js
@@ -0,0 +1,28 @@
+// For Loop- loop will run until end of list
+const names = ["Mack", "Louis", "Eliza","Brick"];
+for (name of names) {
+    if (name === "Mack"){
+        console.log("Mack is in my list!")
+    }
+    console.log(` Hello There ${name}`);
+
+};
+
+const meals = ["Beef Stew", "Chicken Pot Pie", "Chili", "Chicken and Dumplings"];
+
+for (meal of meals){
+    console.log(`For dinner, we will be eating ${meal}`)
+};
+
+
+
+//While Loop
+
+let loading = 0;
+while (loading < 100){
+    console.log("Website is still loading");
+    loading++;
+
+
+
+}
